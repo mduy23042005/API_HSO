122887897c28a3790bdc4d4dbac5a3e3bd027d23
mduy23042005 <lmd23042005@gmail.com>
2025-11-27 11:10:32 +0700

    11:09 AM 27/11/2025 Đưa API lên github.

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+
+public static class WebApiConfig
+{
+    public static void Register(HttpConfiguration config)
+    {
+        // Bật attribute routing
+        config.MapHttpAttributeRoutes();
+
+        // Route mặc định
+        config.Routes.MapHttpRoute(
+            name: "HSOApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+        );
+
+        // Chỉ trả JSON, không trả XML
+        config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+        config.Formatters.JsonFormatter.SupportedMediaTypes
+            .Add(new MediaTypeHeaderValue("application/json"));
+    }
+}
